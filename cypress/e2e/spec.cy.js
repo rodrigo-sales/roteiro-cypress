84cee321598c4b55348a56eaf5a2145dcc98b373
rodrigo-sales <rodrigo.nasc2@gmail.com>
2024-07-02 14:51:04 -0300

    Adicionados testes E2E

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,59 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}');
+
+    cy.get('.todo-list li label')
+      .dblclick();
+
+    cy.get('.todo-list li .edit')
+      .clear()
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software');
+  });
+
+  it('Verifica a limpeza de dados após recarregar a página', () => {
+    cy.visit('http://127.0.0.1:7001');
+  
+    cy.get('.new-todo')
+      .type('Temp Tarefa 1{enter}')
+      .type('Temp Tarefa 2{enter}');
+  
+    cy.get('.todo-list li:nth-child(1) .toggle')
+      .click();
+  
+    cy.reload();
+  
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
+
+  it('Remove todas as tarefas completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
 });
